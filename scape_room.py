29c84fa7d21d7b63d11a3e045a66c695eec8f7d6
juanc1308 <juanc1309@github.com>
2026-06-03 14:05:34 -0600

    Add additional riddle and fix validation logic for user input

--- scape_room.py
+++ scape_room.py
@@ -3,13 +3,13 @@ inventario = []
 
 print("***** ESCAPE ROOM *****")
 nombre = input("\nIngresa tu nombre: ")
-accion = input("\nIngresa enter para continuar: ")
 
 os.system("cls")
 
 llaves = 0
 pista1 = ""
 pista2 = ""
+n_ac = 0
 
 
 print("Ahora se encuentra en una casa abandonada con cuatro puertas, dos puede acceder sin ningún problema, pero las otras dos necesita sus respectivas contraseñas de las puertas, en las cuales podrá obtener mediante las pistas que puede encontrar en dos de las tres habitaciones iniciales.\n")
@@ -22,7 +22,7 @@ while llaves != 5:
 
     opcion = int(input("\nIngrese su opción: "))
 
-    while (opcion < 1) and (opcion > 6):
+    while (opcion < 1) or (opcion > 6):
         opcion = int(input("Ingrese una opción váilda: "))
 
     match opcion:
@@ -31,14 +31,30 @@ while llaves != 5:
             print("--- Cuarto 1 ---")
             print("\n1. Acertijo 1")
             print("2. Acertijo 2")
+            print("3. Acertijo 3")
 
             menu_ac = int(input("\nIngrese el acertijo a resolver: "))
-            while (menu_ac < 1) or (menu_ac > 2):
+            while (menu_ac < 1) or (menu_ac > 3):
                 menu_ac = int(input("Ingrese una opción váilda: "))
-                
-            print("\n- Acertijo 1 -")
-            ac1 = input("Tiene agujas pero no cose. ¿Qué es?")
-            
+            match menu_ac:
+                case 1:
+                    os.system("cls")
+                    print("- Acertijo 1 -")
+                    ac1 = input("Tiene agujas pero no cose. ¿Qué es?: ")
+
+                    if (ac1.lower() == "reloj") or (ac1.lower() == "el reloj"):
+                        n_ac = n_ac + 1
+                        print("Correcto ahora faltan ", (3 - n_ac) ," acertijos más ")
+                        input("Presione enter para continuar: ")
+
+                case 2:
+                    os.system("cls")
+                    print("- Acertijo 2 -")
+                    ac2 = input("Blanca por dentro, verde por fuera. Si quieres que te lo diga, espera.")
+                case 3:
+                    os.system("cls")
+                    print("- Acertijo 3 -")
+                    ac3 = input("Tengo ciudades pero no casas, montañas pero no árboles y agua pero no peces. ¿Qué soy?")
 
         case 2:
             os.system("cls")
